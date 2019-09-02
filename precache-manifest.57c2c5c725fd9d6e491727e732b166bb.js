7cb50cb14732597cb50819eb3204b7f7b44a0f15
Oliver <olsantia@asu.edu>
2019-09-02 13:03:27 -0700

    Updates

---
+++ precache-manifest.57c2c5c725fd9d6e491727e732b166bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1e244a4bf5512ee2cc88bd04292692",
+    "url": "/farmasep/index.html"
+  },
+  {
+    "revision": "7577063736a1d4e2ac5b",
+    "url": "/farmasep/static/css/main.a0082721.chunk.css"
+  },
+  {
+    "revision": "8baac01eab4445c6d34e",
+    "url": "/farmasep/static/js/2.8b4de124.chunk.js"
+  },
+  {
+    "revision": "7577063736a1d4e2ac5b",
+    "url": "/farmasep/static/js/main.23122503.chunk.js"
+  },
+  {
+    "revision": "43969b147b55ff2b26f2",
+    "url": "/farmasep/static/js/runtime~main.a7d5bf0b.js"
+  },
+  {
+    "revision": "97c5e61afb48222f230b542b958b2378",
+    "url": "/farmasep/static/media/Resources.97c5e61a.svg"
+  },
+  {
+    "revision": "038567d4e9b0de72592d2dfb767feef1",
+    "url": "/farmasep/static/media/farmasep-logo.038567d4.svg"
+  },
+  {
+    "revision": "a27b9ed6f33bf32233b77a5d79790586",
+    "url": "/farmasep/static/media/offers.a27b9ed6.svg"
+  },
+  {
+    "revision": "d0842a453b07d6898e6f083a0a2f60a0",
+    "url": "/farmasep/static/media/questions.d0842a45.svg"
+  }
+]);
